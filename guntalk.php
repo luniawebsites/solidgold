3939da441c2670e8cca08abf60318f1a3b560a73
Gavin Kennedy <gavin@kennedy.co.za>
2019-02-21 16:50:32 +0200

    Update guntalk.php

--- guntalk.php
+++ guntalk.php
@@ -140,9 +140,10 @@
 				<!-- <iframe src="https://tunein.com/embed/player/p1179805/" style="width:100%; height:100px;" scrolling="no" frameborder="no"></iframe> -->
 				
 					<div class="pad-y-mob h2 text-c-mob text-r-tab">LISTEN NOW by selecting your favourite player below</div>
-					<!-- <a class="badge-apple-podcasts" href="https://itunes.apple.com/gb/podcast/lights-cameras-action-with-kimberleigh-stark/id1445292881" target="_blank" rel="noopener"></a>
-					<a class="badge-stitcher" href="https://www.stitcher.com/podcast/solid-gold-studios/lights-cameras-action-with-kimberleigh-stark" target="_blank" rel="noopener"></a>
-					<a class="badge-acast" href="https://www.acast.com/famous" target="_blank" rel="noopener"></a>
+					<!-- <a class="badge-apple-podcasts" href="https://itunes.apple.com/gb/podcast/lights-cameras-action-with-kimberleigh-stark/id1445292881" target="_blank" rel="noopener"></a> -->
+					<a class="badge-stitcher" href="https://www.stitcher.com/s?fid=375470&refid=stpr" target="_blank" rel="noopener"></a>
+					<a class="badge-acast" href="https://www.acast.com/gosaguntalk" target="_blank" rel="noopener"></a>
+					<!--
 					<a class="badge-google-podcasts" href="https://play.google.com/music/m/I5verrs3l6ikb3dv7lisuttlloe?t=Lights_Cameras_Action_with_Kimberleigh_Stark" target="_blank" rel="noopener"></a>
 					<a class="badge-tune-in" href="https://tunein.com/podcasts/Arts--Culture-Podcasts/Lights-Cameras-Action-with-Kimberleigh-Stark-p1179805/" target="_blank" rel="noopener"></a> -->
 					<a class="badge-castbox" href="https://castbox.fm/ch/2024398?country=za" target="_blank" rel="noopener"></a>

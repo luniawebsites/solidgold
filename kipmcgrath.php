04d0e9ddea6f36dc39f5e44d9733c3c244ade1db
Gavin Kennedy <gavin@kennedy.co.za>
2019-03-09 12:07:29 +0200

    Update kipmcgrath.php

--- kipmcgrath.php
+++ kipmcgrath.php
@@ -146,6 +146,7 @@
 
 <div class="pad-y-mob h2 text-c-mob text-r-tab">Or you can select your preferred podcast player below</div>
 <a class="badge-castbox" href="https://castbox.fm/channel/id2045311" target="_blank" rel="noopener"></a>
+<a class="badge-spotify" href="https://open.spotify.com/show/0IA0CcMwdX7sZhcHW5lt9e" target="_blank" rel="noopener"></a>
 
 <a class="badge-rss-feed" href="https://chtbl.com/track/podcaststudios.co.za/rss/kipmcgrath.xml" target="_blank" rel="noopener"></a>
 

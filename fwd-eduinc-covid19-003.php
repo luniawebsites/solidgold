655d8f1cb629be1e788d7b1d169316da602824b3
Gavin Kennedy <gavin@kennedy.co.za>
2020-03-30 19:29:51 +0200

    Create fwd-eduinc-covid19-003.php

---
+++ fwd-eduinc-covid19-003.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+
+<!--
+PARTIAL: Google Tag Manager script
+-->
+<?php include("assets/partials/google-tag-manager-script.html"); ?>
+
+<title>EduInc Covid19 Podcast - Ep 3 | another Solid Gold podcast</title>
+
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<meta name="keywords" content="eduinc education incorporated jax gersh aitchison school covid19 virus pandemic podcast gavin kennedy another solid gold podcast">
+<meta name="description" content="To be understood, we must first #BeHeard.">
+<link rel="stylesheet" href="styles/order.css" type="text/css">
+</head>
+
+<body>
+
+<!--
+PARTIAL: Google Tag Manager body
+-->
+<?php include("assets/partials/google-tag-manager-body.html"); ?>
+
+<meta http-equiv="refresh" content="1; url=https://podcaststudios.co.za/audio/eduinc-covid19/eduinc-covid19-003.mp3">
+
+</body>
+</html>

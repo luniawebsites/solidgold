2ce1e9e21452d4f3497d0bda772452ce15715eaf
Gavin Kennedy <gavin@kennedy.co.za>
2020-05-14 18:11:30 +0200

    bry hi 003

---
+++ fwd-bryhi-003.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+
+<!--
+PARTIAL: Google Tag Manager script
+-->
+<?php include("assets/partials/google-tag-manager-script.html"); ?>
+
+<title>Bryanston High Podcast | Covid-19 | another Solid Gold podcast</title>
+
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<meta name="keywords" content="bryanston high school john skelton covid19 virus pandemic podcast gavin kennedy another solid gold podcast">
+<meta name="description" content="To be understood, we must first #BeHeard.">
+<link rel="stylesheet" href="styles/order.css" type="text/css">
+</head>
+
+<body>
+
+<!--
+PARTIAL: Google Tag Manager body
+-->
+<?php include("assets/partials/google-tag-manager-body.html"); ?>
+
+<meta http-equiv="refresh" content="1; url=https://chtbl.com/track/85158/podcaststudios.co.za/audio/blue-flame/blue-flame-003-covid-19-update-01.mp3">
+
+</body>
+</html>

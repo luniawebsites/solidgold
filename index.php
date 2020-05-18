3728c550977e0a5020f4cb0afb123706e7ca0adf
Gavin Kennedy <gavin@kennedy.co.za>
2020-05-18 15:07:48 +0200

    cringe

--- index.php
+++ index.php
@@ -106,9 +106,9 @@ HEADER: Hero
 
 			<!-- <p>Founded in 1993, we help our <a href="channels.php" target="_blank" rel="noopener" id="header_hero_a_podcastchannels" aria-labelledby="header_hero_a_podcastchannels"><abbr title="Clients">clients</abbr></a> to stand out from the crowd by producing their own podcasts. Listen to this interview if you are <a href="https://castbox.fm/app/castbox/player/id1478229/id185658959?v=8.10.3&autoplay=0" target="_blank" title="begin" rel="noopener" id="header_hero_a_begin" aria-labelledby="header_hero_a_begin"><abbr title="Where to begin?">wondering where to begin</abbr></a>.</p> -->
 
-			<p>Are you caught in the lock-down? Then read our helpful article on <a href="https://landing.mailerlite.com/webforms/landing/w5t4g5" target="_blank" rel="noopener" id="header_hero_a_pfh" aria-labelledby="header_hero_a_pfh"><abbr title="Podcast From Home">how to sound your best when recording at home</abbr></a>.</p>
+			<p>Are you working from home? Our article on <a href="https://landing.mailerlite.com/webforms/landing/w5t4g5" target="_blank" rel="noopener" id="header_hero_a_pfh" aria-labelledby="header_hero_a_pfh"><abbr title="Podcast From Home">how to sound your best</abbr></a> will help when recording or presenting at home. Ever wondered why you <a href="https://landing.mailerlite.com/webforms/landing/m8m6z8" target="_blank" rel="noopener" id="header_hero_a_pfh" aria-labelledby="header_hero_a_cringe"><abbr title="Why Do I Cringe">cringe at the sound of your own voice?</abbr></a></p>
 
-			<p>New to podcasting? Listen to this interview if you are <a href="https://castbox.fm/app/castbox/player/id1478229/id185658959?v=8.10.3&autoplay=0" target="_blank" title="begin" rel="noopener" id="header_hero_a_begin" aria-labelledby="header_hero_a_begin"><abbr title="Where to begin?">wondering where to begin</abbr></a>.</p>
+			<p>New to podcasting? Listen to this interview if you are <a href="https://castbox.fm/app/castbox/player/id1478229/id185658959?v=8.10.3&autoplay=0" target="_blank" title="begin" rel="noopener" id="header_hero_a_begin" aria-labelledby="header_hero_a_begin"><abbr title="Where to begin?">searching for information on where and how to begin</abbr></a>.</p>
 
 			<p>We also provide Corporate <strong>Private Podcasts</strong> in partnership with</p>
 

de3de4a9180fd1e3c6103d5d8596c5e3d61c1685
Gavin Kennedy <gavin@kennedy.co.za>
2020-05-20 18:36:11 +0200

    bry hi ep 04

---
+++ fwd-bryhi-004.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+
+<!--
+PARTIAL: Google Tag Manager script
+-->
+<?php include("assets/partials/google-tag-manager-script.html"); ?>
+
+S
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<meta name="keywords" content="bryanston high school john skelton aidan hillebrand gill fulcher covid19 virus pandemic podcast gavin kennedy another solid gold podcast">
+<meta name="description" content="To be understood, we must first #BeHeard.">
+<link rel="stylesheet" href="styles/order.css" type="text/css">
+</head>
+
+<body>
+
+<!--
+PARTIAL: Google Tag Manager body
+-->
+<?php include("assets/partials/google-tag-manager-body.html"); ?>
+
+<meta http-equiv="refresh" content="1; url=https://chtbl.com/track/85158/podcaststudios.co.za/audio/blue-flame/blue-flame-004-back-to-school.mp3">
+
+</body>
+</html>

d92819afd317da2913e8032bc58a555beca89fe5
Gavin Kennedy <gavin@kennedy.co.za>
2020-06-13 11:07:33 +0200

    maredi

---
+++ fwd-eduinc-covid19-007.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+
+<!--
+PARTIAL: Google Tag Manager script
+-->
+<?php include("assets/partials/google-tag-manager-script.html"); ?>
+
+<title>EduInc Covid19 Podcast - Ep 7 | another Solid Gold podcast</title>
+
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<meta name="keywords" content="eduinc education incorporated jax gersh aitchison school covid19 virus pandemic udemy podcast gavin kennedy another solid gold podcast">
+<meta name="description" content="To be understood, we must first #BeHeard.">
+<link rel="stylesheet" href="styles/order.css" type="text/css">
+</head>
+
+<body>
+
+<!--
+PARTIAL: Google Tag Manager body
+-->
+<?php include("assets/partials/google-tag-manager-body.html"); ?>
+
+<meta http-equiv="refresh" content="1; url=https://chtbl.com/track/85158/podcaststudios.co.za/audio/eduinc/edu-inc-016-covid19-pay-attention-matrics.mp3">
+
+</body>
+</html>

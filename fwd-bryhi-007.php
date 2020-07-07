7f4b6212bfca59de69baf674e1f00d1a15e57d03
Gavin Kennedy <gavin@kennedy.co.za>
2020-07-07 10:00:16 +0200

    use cases fix

---
+++ fwd-bryhi-007.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+
+<!--
+PARTIAL: Google Tag Manager script
+-->
+<?php include("assets/partials/google-tag-manager-script.html"); ?>
+
+S
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<meta name="keywords" content="bryanston high school john skelton gia marais bev weir covid19 virus pandemic podcast gavin kennedy another solid gold podcast">
+<meta name="description" content="To be understood, we must first #BeHeard.">
+<link rel="stylesheet" href="styles/order.css" type="text/css">
+</head>
+
+<body>
+
+<!--
+PARTIAL: Google Tag Manager body
+-->
+<?php include("assets/partials/google-tag-manager-body.html"); ?>
+
+<meta http-equiv="refresh" content="1; url=https://podcasts.google.com/feed/aHR0cHM6Ly9wb2RjYXN0c3R1ZGlvcy5jby56YS9yc3MvYmx1ZS1mbGFtZS54bWw=/episode/Ymx1ZS1mbGFtZS0wMDc=">
+
+</body>
+</html>

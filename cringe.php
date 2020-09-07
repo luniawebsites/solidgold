f84950be7b8a39ae14a5516c4f0b6f5a5ec9bc4b
Gavin Kennedy <gavin@kennedy.co.za>
2020-09-07 08:35:34 +0200

    Create cringe.php

---
+++ cringe.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+
+<!--
+PARTIAL: Google Tag Manager script
+-->
+<?php include("assets/partials/google-tag-manager-script.html"); ?>
+
+<title>Why do I dislike the sound of my own voice? | Solid Gold Podcasts</title>
+
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<meta name="keywords" content="solid gold podcast studios dislike hate sound of my own voice cringe">
+<meta name="description" content="To be understood, we must first #BeHeard.">
+<link rel="stylesheet" href="styles/order.css" type="text/css">
+</head>
+
+<body>
+
+<!--
+PARTIAL: Google Tag Manager body
+-->
+<?php include("assets/partials/google-tag-manager-body.html"); ?>
+
+<meta http-equiv="refresh" content="1; url=https://preview.mailerlite.com/p0w3g7">
+
+</body>
+</html>

a67c73a7ae7e15461d0e848ed9d99492a4588144
Gavin Kennedy <gavin@kennedy.co.za>
2020-09-07 12:56:22 +0200

    add pfh and remote

---
+++ pfh.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+
+<!--
+PARTIAL: Google Tag Manager script
+-->
+<?php include("assets/partials/google-tag-manager-script.html"); ?>
+
+<title>Podcasting from Home - Tips n Tricks | Solid Gold Podcasts</title>
+
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<meta name="keywords" content="solid gold podcast studios podcasting from home tips tricks mic room">
+<meta name="description" content="To be understood, we must first #BeHeard.">
+<link rel="stylesheet" href="styles/order.css" type="text/css">
+</head>
+
+<body>
+
+<!--
+PARTIAL: Google Tag Manager body
+-->
+<?php include("assets/partials/google-tag-manager-body.html"); ?>
+
+<meta http-equiv="refresh" content="1; url=https://preview.mailerlite.com/m3e4p9">
+
+</body>
+</html>
---
+++ remote.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+
+<!--
+PARTIAL: Google Tag Manager script
+-->
+<?php include("assets/partials/google-tag-manager-script.html"); ?>
+
+<title>Let's make a Podcast! (remotely) | Solid Gold Podcasts</title>
+
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<meta name="keywords" content="solid gold podcast studios remote studio squadcast riverside welder">
+<meta name="description" content="To be understood, we must first #BeHeard.">
+<link rel="stylesheet" href="styles/order.css" type="text/css">
+</head>
+
+<body>
+
+<!--
+PARTIAL: Google Tag Manager body
+-->
+<?php include("assets/partials/google-tag-manager-body.html"); ?>
+
+<meta http-equiv="refresh" content="1; url=https://docs.google.com/document/d/1pL4rfHDyLFHTD24CK0fiF9Bh5a2bN7WiYiPLbQGltgQ/edit?usp=sharing">
+
+</body>
+</html>

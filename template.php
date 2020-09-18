945dc176b30a225c28891dc4a9641ecd8b99a965
Gavin Kennedy <gavin@kennedy.co.za>
2020-09-18 18:40:39 +0200

    Create template.php

---
+++ template.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+
+<!--
+PARTIAL: Google Tag Manager script
+-->
+<?php include("assets/partials/google-tag-manager-script.html"); ?>
+
+<title>Before you record your first podcast... | Solid Gold Podcasts</title>
+
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<meta name="keywords" content="solid gold podcast studios template plan prepare">
+<meta name="description" content="To be understood, we must first #BeHeard.">
+<link rel="stylesheet" href="styles/order.css" type="text/css">
+</head>
+
+<body>
+
+<!--
+PARTIAL: Google Tag Manager body
+-->
+<?php include("assets/partials/google-tag-manager-body.html"); ?>
+
+<meta http-equiv="refresh" content="1; url=https://docs.google.com/presentation/d/e/2PACX-1vQtAr6DOBWTMYCoeWqVH1Wet2yc16HBueDaWSgmanT5ssP1AxwTka2QJQxvd6CYpxrTL0bNxaMbC93v/pub?start=false&loop=false&delayms=60000&slide=id.p">
+
+</body>
+</html>

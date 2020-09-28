71f8c42db82eaad04612fd61022ba3ff323f34de
Gavin Kennedy <gavin@kennedy.co.za>
2020-09-28 09:33:28 +0200

    Create share.php

---
+++ share.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+
+<!--
+PARTIAL: Google Tag Manager script
+-->
+<?php include("assets/partials/google-tag-manager-script.html"); ?>
+
+<title>How to Share, Link and EMbed my podcasts | Solid Gold Podcasts</title>
+
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<meta name="keywords" content="solid gold podcast studios link share embed">
+<meta name="description" content="To be understood, we must first #BeHeard.">
+<link rel="stylesheet" href="styles/order.css" type="text/css">
+</head>
+
+<body>
+
+<!--
+PARTIAL: Google Tag Manager body
+-->
+<?php include("assets/partials/google-tag-manager-body.html"); ?>
+
+<meta http-equiv="refresh" content="1; url=https://docs.google.com/document/d/19sERQdj-N0kXByPeaQE-x-gkNbn0ZIZPUrZGJLr-FNk/edit?usp=sharing">
+
+</body>
+</html>

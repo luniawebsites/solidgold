96f8e2166469c6cae9ffd623646fa3d6dfec6123
Gavin Kennedy <gavin@kennedy.co.za>
2020-09-29 07:31:50 +0200

    limitless and eduinc

---
+++ fwd-eduinc-covid19-011.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+
+<!--
+PARTIAL: Google Tag Manager script
+-->
+<?php include("assets/partials/google-tag-manager-script.html"); ?>
+
+<title>EduInc Podcast - Matric Valediction | another Solid Gold podcast</title>
+
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<meta name="keywords" content="eduinc education incorporated jax gersh aitchison school covid19 virus pandemic udemy podcast gavin kennedy another solid gold podcast">
+<meta name="description" content="To be understood, we must first #BeHeard.">
+<link rel="stylesheet" href="styles/order.css" type="text/css">
+</head>
+
+<body>
+
+<!--
+PARTIAL: Google Tag Manager body
+-->
+<?php include("assets/partials/google-tag-manager-body.html"); ?>
+
+<meta http-equiv="refresh" content="1; url=https://podcasts.google.com/feed/aHR0cHM6Ly9wb2RjYXN0c3R1ZGlvcy5jby56YS9yc3MvZWR1aW5jY3JlYXRpdmVhcnRzLnhtbA==/episode/ZWR1aW5jLTAyNQ==">
+
+</body>
+</html>

3b747aecd6b593d14dc0038c7e0ddb289859f3c0
Gavin Kennedy <gavin@kennedy.co.za>
2020-10-08 12:07:16 +0200

    odds n sods

---
+++ job.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+
+<!--
+PARTIAL: Google Tag Manager script
+-->
+<?php include("assets/partials/google-tag-manager-script.html"); ?>
+
+<title>Submit Job Card for Work Done | Solid Gold Podcasts</title>
+
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<meta name="keywords" content="solid gold podcast studios job card work done">
+<meta name="description" content="To be understood, we must first #BeHeard.">
+<link rel="stylesheet" href="styles/order.css" type="text/css">
+</head>
+
+<body>
+
+<!--
+PARTIAL: Google Tag Manager body
+-->
+<?php include("assets/partials/google-tag-manager-body.html"); ?>
+
+<meta http-equiv="refresh" content="1; url=https://tiny.cc/sgsjob">
+
+</body>
+</html>

4caf2c31acd30f4ccdab1a6a7e178f6cefc52052
Gavin Kennedy <gavin@kennedy.co.za>
2020-10-26 07:41:28 +0200

    tep

---
+++ fwd-bryhi-009.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+
+<!--
+PARTIAL: Google Tag Manager script
+-->
+<?php include("assets/partials/google-tag-manager-script.html"); ?>
+
+<title>Bryanston High | Welcome Grade 8s of 2021</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<meta name="keywords" content="bryanston high school John Skelton Principal Carryn Bukris Adam Mawdsley Milo Ntshanga  Shea Fourie Grade Controllers grade 8 2021 podcast gavin kennedy another solid gold podcast">
+<meta name="description" content="To be understood, we must first #BeHeard | another Solid Gold podcast">
+<link rel="stylesheet" href="styles/order.css" type="text/css">
+</head>
+
+<body>
+
+<!--
+PARTIAL: Google Tag Manager body
+-->
+<?php include("assets/partials/google-tag-manager-body.html"); ?>
+
+<meta http-equiv="refresh" content="1; url=https://pod.link/1484452333/episode/Ymx1ZS1mbGFtZS0wMDk=">
+
+</body>
+</html>

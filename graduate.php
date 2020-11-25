64bbceea129661d8fe9ed9f9b8e282188c508727
Gavin Kennedy <gavin@kennedy.co.za>
2020-11-25 10:34:51 +0200

    wcr

---
+++ graduate.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+
+<!--
+PARTIAL: Google Tag Manager script
+-->
+<?php include("assets/partials/google-tag-manager-script.html"); ?>
+
+<title>Workplace Career Readiness | #BeHeard another Solid Gold podcast</title>
+
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<meta name="keywords" content="Workplace Career Readiness graduade intern Thamsanqa Maqubela Rinus le Roux SACGRA gavin kennedy another solid gold podcast #BeHeard">
+<meta name="description" content="To be understood, we must first #BeHeard.">
+<link rel="stylesheet" href="styles/order.css" type="text/css">
+</head>
+
+<body>
+
+<!--
+PARTIAL: Google Tag Manager body
+-->
+<?php include("assets/partials/google-tag-manager-body.html"); ?>
+
+<meta http-equiv="refresh" content="1; url=https://pod.link/1540905767">
+
+</body>
+</html>

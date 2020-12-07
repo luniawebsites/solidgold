9b4e778d1ca8dce8b670b755c960c349f790f984
Gavin Kennedy <gavin@kennedy.co.za>
2020-12-07 11:38:51 +0200

    moon

---
+++ subscribe-moon-success.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<meta charset="utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=3.0, minimum-scale=0.5, user-scalable=yes" />
+
+<!--
+PARTIAL: Base URL
+-->
+<?php include("assets/partials/base-url.html"); ?>
+
+<!--
+PARTIAL: Google Tag Manager script
+-->
+<?php include("assets/partials/google-tag-manager-script.html"); ?>
+
+<!-- 
+LINK/PARTIAL: Defer load non-critical CSS
+-->
+<link rel="stylesheet" href="assets/non-critical.min.css" media="print" onload="this.media='all'">
+
+<meta name="robots" content="noindex, nofollow" />
+<meta name="pagename" content="" />
+<meta name="HandheldFriendly" content="true" />
+<meta name="MobileOptimized" content="320" />
+<title>How do photos of the moon improve podcasts?</title>
+<link rel="icon" type="image/png" sizes="32x32" href="favicon.png">
+<link rel="apple-touch-icon" sizes="512x512" href="apple-touch-icon.png">
+
+<!--
+STYLES: Inline load critical CSS
+-->
+<style>
+	<?php include("assets/critical.min.css"); ?>
+</style>
+
+<!--
+PARTIAL: Google Tag Manager body
+-->
+<?php include("assets/partials/google-tag-manager-body.html"); ?>
+
+<!--
+PARTIAL: Leader with menu
+-->
+<?php include("assets/partials/pattern-page-leader.html"); ?>
+
+<!--
+MAIN
+-->
+
+<div role="main"> <!-- This is used instead of <main> to ensure CSS grid works -->
+
+	<!--
+	ARTICLE: Contact success
+	-->
+
+	<article class="pad-respond-xy-mob theme-light-grey-mob">
+
+		<!--
+		LAYOUT CONTAINER
+		-->
+
+		<div class="flexdir-btt-mob flexdir-rtl-tab flexpos-lc-tab constrain spread-respond pad-y-mob">
+
+			<!--
+			CONTENT
+			-->
+
+			<div>
+
+				<h1>Welcome!</h1>
+
+				<p>Our mail that explains <strong>what you can learn about podcasting from your photos of the moon</strong> is on its way to you.</p>
+				<p>I hope it adds some value.</p>
+				<p>Please do share any of your podcast  stories on our <a href="https://www.facebook.com/solidgoldpodcaststudios/" target="_blank" rel="noopener" id="pfh_subscribe" aria-labelledby="pfh_subscribe">Facebook page</a></p>
+				<p><strong>#BeHeard</strong></p>
+		
+			</div>
+
+		</div>
+
+	</article>
+
+</div>
+
+<!--
+PARTIAL: Page footer
+-->
+<?php include("assets/partials/pattern-page-footer.html"); ?>
+
+
+<!--
+SCRIPT: jQuery scripts
+-->
+<script src="assets/scripts/jquery/jquery.min.js"></script>
+<script src="assets/scripts/jquery/pushy.min.js" defer></script>
+
+</html>

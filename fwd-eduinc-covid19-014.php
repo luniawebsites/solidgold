479127a090b784583bbf71ff95ce52795f8383b7
Gavin Kennedy <gavin@kennedy.co.za>
2021-01-07 21:39:05 +0200

    edu

---
+++ fwd-eduinc-covid19-014.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+
+<!--
+PARTIAL: Google Tag Manager script
+-->
+<?php include("assets/partials/google-tag-manager-script.html"); ?>
+
+<title>Welcome to 2021 - When? How? What? | #BeHeard another Solid Gold podcast</title>
+
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<meta name="keywords" content="eduinc education incorporated jax gersh aitchison school covid19 virus pandemic udemy podcast gavin kennedy when does school open another solid gold podcast">
+<meta name="description" content="To be understood, we must first #BeHeard.">
+<link rel="stylesheet" href="styles/order.css" type="text/css">
+</head>
+
+<body>
+
+<!--
+PARTIAL: Google Tag Manager body
+-->
+<?php include("assets/partials/google-tag-manager-body.html"); ?>
+
+<meta http-equiv="refresh" content="1; url=https://chtbl.com/track/85158/podcasters.co.za/audio/eduinc/edu-inc-028-welcome-to-2021.mp3?ver=1">
+
+</body>
+</html>
